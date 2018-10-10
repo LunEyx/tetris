ba865619e3d54778e3cdb26a96fae57207600ece
LunEyx <luneyxlab@gmail.com>
2018-10-10 23:22:10 +0800

    Updates

---
+++ precache-manifest.d44a5b4ba96cec6f6abf6cd5cf286dc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0277f3ed11c442e22cbb",
+    "url": "/tetris/static/js/runtime~main.0277f3ed.js"
+  },
+  {
+    "revision": "73362cbcb6b0a6905edc",
+    "url": "/tetris/static/js/main.73362cbc.chunk.js"
+  },
+  {
+    "revision": "5a78ad88eb54edf29ecc",
+    "url": "/tetris/static/js/1.5a78ad88.chunk.js"
+  },
+  {
+    "revision": "73362cbcb6b0a6905edc",
+    "url": "/tetris/static/css/main.dfb03c3b.chunk.css"
+  },
+  {
+    "revision": "eb7c54a4944d3b73b9431bc8127008f8",
+    "url": "/tetris/index.html"
+  }
+];
